b354de85f7557211c714c82b6d9103b24938c2bc
ryu <flavcast.job@gmail.com>
2023-09-29 04:06:17 +0200

    Add new field in form. Update 'handleSubmit' function to correspond with form. Rename few variables. Begin to implement 'resolve' function.

--- src/pages/advice.jsx
+++ src/pages/advice.jsx
@@ -1,9 +1,9 @@
 import { useState } from "react";
 
 export function Advice() {
+  const [otherList, setOther] = useState([]);
   const [progList, setProg] = useState([]);
   const [qualList, setQual] = useState([]);
-  const [geneList, setGene] = useState([]);
 
   const virtualDurability = (buffs) => {
     return buffs.dura + 5 * buffs.wn + 5 * buffs.manip;
@@ -14,7 +14,6 @@ export function Advice() {
 
     let buffs = {
       goalProg: event.target.goalProg.checked,
-      goalIq: event.target.goalIq.checked,
       goalQual: event.target.goalQual.checked,
 
       cond: event.target.cond.value,
@@ -27,6 +26,7 @@ export function Advice() {
       manip: Number(event.target.manip.value),
       wn: Number(event.target.wn.value),
       obs: event.target.obs.checked,
+      iq: Number(event.target.iq.value),
       innov: Number(event.target.innov.value),
       gs: Number(event.target.gs.value),
     };
@@ -35,14 +35,234 @@ export function Advice() {
   };
 
   const resolve = (buffs) => {
-    let gene = [];
+    let other = [];
     let prog = [];
     let qual = [];
     let vdur = virtualDurability(buffs);
+    console.log(vdur);
 
+    //Repair TODO:value dura, vdur | affinate
+    if (buffs.dura <= 10 || vdur < 10) {
+      switch (buffs.cond) {
+        case "normal":
+          other.push("Master's Mend");
+          other.push("Manipulation");
+          break;
+
+        case "centered":
+          other.push("Master's Mend");
+          other.push("Manipulation");
+          break;
+
+        case "omen":
+          other.push("Master's Mend");
+          other.push("Manipulation");
+          break;
+
+        case "good":
+          other.push("Tricks of Trade");
+          break;
+
+        case "malleable":
+          other.push("Master's Mend");
+          other.push("Manipulation");
+          break;
+
+        case "pliant":
+          other.push("Master's Mend");
+          other.push("Manipulation");
+          break;
+
+        case "primed":
+          other.push("Manipulation");
+          other.push("Master's Mend");
+          break;
+
+        case "sturdy":
+          other.push("Master's Mend");
+          other.push("Manipulation");
+          break;
+
+        default:
+          break;
+      }
+    } else {
+      // Prog goal
+      if (buffs.goalProg) {
+        // Opener Mume
+        if (buffs.mume > 0) {
+          switch (buffs.cond) {
+            case "normal":
+              if (buffs.mume == 5) {
+                prog.push("Veneration");
+              } else {
+                prog.push("Rapid Synthesis");
+              }
+              break;
+
+            case "centered":
+              if (buffs.mume == 5) {
+                prog.push("Veneration");
+              } else {
+                prog.push("Rapid Synthesis");
+              }
+              break;
+
+            case "omen":
+              if (buffs.mume == 5) {
+                prog.push("Veneration");
+              } else {
+                prog.push("Rapid Synthesis");
+              }
+              break;
+
+            case "good":
+              qual.push("Precise Touch");
+              break;
+
+            case "malleable":
+              if (buffs.mume == 5) {
+                prog.push("Veneration");
+              } else {
+                prog.push("Rapid Synthesis");
+              }
+              break;
+
+            case "pliant":
+              // TODO
+              if (buffs.mume == 5) {
+                prog.push("Veneration");
+              } else if (buffs.manip == 0) {
+                other.push("Manipulation");
+              } else {
+                prog.push("Rapid Synthesis");
+              }
+              break;
+
+            case "primed":
+              if (buffs.mume == 5) {
+                prog.push("Veneration");
+              } else if (buffs.manip == 0) {
+                other.push("Manipulation");
+              } else {
+                prog.push("Rapid Synthesis");
+              }
+              break;
+
+            case "sturdy":
+              if (buffs.mume == 5) {
+                prog.push("Veneration");
+              } else {
+                prog.push("Rapid Synthesis");
+              }
+              break;
+
+            default:
+              break;
+          }
+        }
+        // Prog after opener
+        else {
+          switch (buffs.cond) {
+            case "normal":
+              break;
+
+            case "centered":
+              break;
+
+            case "omen":
+              break;
+
+            case "good":
+              other.push("Tricks of Trade");
+              prog.push("Intensive Synthesis");
+              break;
+
+            case "malleable":
+              prog.push("Rapid Synthesis");
+              break;
+
+            case "pliant":
+              break;
+
+            case "primed":
+              break;
+
+            case "sturdy":
+              break;
+
+            default:
+              break;
+          }
+        }
+      }
+    }
+
+    // Quality goal
+    if (buffs.goalQual) {
+      switch (buffs.cond) {
+        case "normal":
+          break;
+
+        case "centered":
+          break;
+
+        case "omen":
+          break;
+
+        case "good":
+          break;
+
+        case "malleable":
+          break;
+
+        case "pliant":
+          break;
+
+        case "primed":
+          break;
+
+        case "sturdy":
+          break;
+
+        default:
+          break;
+      }
+    }
+
+    setOther(other);
     setProg(prog);
     setQual(qual);
-    setGene(gene);
+
+    /*switch (buffs.cond) {
+          case "normal":
+            break;
+
+          case "centered":
+            break;
+
+          case "omen":
+            break;
+
+          case "good":
+            break;
+
+          case "malleable":
+            break;
+
+          case "pliant":
+            break;
+
+          case "primed":
+            break;
+
+          case "sturdy":
+            break;
+
+          default:
+            break;
+        }
+        */
   };
 
   return (
@@ -54,11 +274,6 @@ export function Advice() {
           <label>
             Progress:
             <input type="checkbox" name="goalProg" />
-          </label>
-
-          <label>
-            Inner Quiet Stack:
-            <input type="checkbox" name="goalIq" />
           </label>
 
           <label>
@@ -174,6 +389,23 @@ export function Advice() {
           </label>
 
           <label>
+            Inner Quiet:
+            <select name="iq">
+              <option value="0">0</option>
+              <option value="1">1</option>
+              <option value="2">2</option>
+              <option value="3">3</option>
+              <option value="4">4</option>
+              <option value="5">5</option>
+              <option value="6">6</option>
+              <option value="7">7</option>
+              <option value="8">8</option>
+              <option value="9">9</option>
+              <option value="10">10</option>
+            </select>
+          </label>
+
+          <label>
             Innovation:
             <select name="innov">
               <option value="0">0</option>
@@ -205,7 +437,7 @@ export function Advice() {
       <section>
         <ul>
           <h1>General</h1>
-          {geneList.map((item) => (
+          {otherList.map((item) => (
             <li>{item}</li>
           ))}
         </ul>
